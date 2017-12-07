4be0a31f1e621a9247e0d75188400163639fdfe5
rhpvorderman <r.h.p.vorderman@lumc.nl>
2017-12-07 09:46:23 +0100

    Proper methods for settings and tasks

--- src/main/scala/nl/biopet/sbtbiopet/BiopetPlugin.scala
+++ src/main/scala/nl/biopet/sbtbiopet/BiopetPlugin.scala
@@ -1,23 +1,15 @@
 package nl.biopet.sbtbiopet
 
-import sbt._
+import sbt.{Def, _}
 import Keys._
 import com.typesafe.sbt.site.SitePlugin
-import com.typesafe.sbt.site.SitePlugin.autoImport.{
-  siteDirectory,
-  siteSubdirName,
-  makeSite
-}
+import com.typesafe.sbt.site.SitePlugin.autoImport.{makeSite, siteDirectory, siteSubdirName}
 import com.typesafe.sbt.site.laika.LaikaSitePlugin
 import com.typesafe.sbt.site.laika.LaikaSitePlugin.autoImport.LaikaSite
 import com.typesafe.sbt.site.SiteScaladocPlugin
 import com.typesafe.sbt.site.SiteScaladocPlugin.autoImport.SiteScaladoc
 import com.typesafe.sbt.sbtghpages.GhpagesPlugin
-import com.typesafe.sbt.sbtghpages.GhpagesPlugin.autoImport.{
-  ghpagesRepository,
-  ghpagesCleanSite,
-  ghpagesPushSite
-}
+import com.typesafe.sbt.sbtghpages.GhpagesPlugin.autoImport.{ghpagesCleanSite, ghpagesPushSite, ghpagesRepository}
 import laika.sbt.LaikaSbtPlugin.LaikaKeys.{Laika, rawContent}
 import sbtassembly.AssemblyPlugin
 import sbtassembly.AssemblyPlugin.autoImport.assembly
@@ -26,11 +18,7 @@ import com.typesafe.sbt.SbtPgp
 import com.typesafe.sbt.SbtPgp.autoImport.useGpg
 import sbtrelease.ReleasePlugin
 import ReleasePlugin.autoImport.ReleaseTransformations._
-import ReleasePlugin.autoImport.{
-  ReleaseStep,
-  releaseProcess,
-  releaseStepCommand
-}
+import ReleasePlugin.autoImport.{ReleaseStep, releaseProcess, releaseStepCommand}
 
 object BiopetPlugin extends AutoPlugin {
   override val requires: Plugins = {
@@ -50,7 +38,7 @@ object BiopetPlugin extends AutoPlugin {
   def BiopetGlobalSettings: Seq[Setting[_]] = Seq(
     // Publication to nexus repository
     resolvers += Resolver.sonatypeRepo("snapshots"),
-    publishTo := biopetPublishTo,
+    publishTo := biopetPublishTo.value,
     useGpg := true,
     // Jar assembly
     releaseProcess := biopetReleaseProcess,
@@ -64,9 +52,9 @@ object BiopetPlugin extends AutoPlugin {
       if (isSnapshot.value) { "develop/api" } else s"${version.value}/api"
     },
     rawContent := true, //Laika use raw HTML content in markdown.
-    includeFilter in ghpagesCleanSite := biopetCleanSiteFilter,
-    biopetGenerateDocs := biopetGenerateDocsFunction,
-    biopetGenerateReadme := biopetGenerateReadmeFunction,
+    includeFilter in ghpagesCleanSite := biopetCleanSiteFilter.value,
+    biopetGenerateDocs := biopetGenerateDocsFunction().value,
+    biopetGenerateReadme := biopetGenerateReadmeFunction().value,
     makeSite := (makeSite triggeredBy biopetGenerateDocs).value,
     makeSite := (makeSite dependsOn biopetGenerateDocs).value,
     ghpagesPushSite := (ghpagesPushSite dependsOn makeSite).value
@@ -80,8 +68,8 @@ object BiopetPlugin extends AutoPlugin {
     },
     git.remoteRepo := s"git@github.com:biopet/${biopetUrlName.value}.git"
   )
-  private def biopetPublishTo: Option[Resolver] = {
-    val nexus = "https://oss.sonatype.org/"
+  private def biopetPublishTo: Def.Initialize[Option[Resolver]] =
+  Def.setting {
     if (isSnapshot.value)
       Some(Opts.resolver.sonatypeSnapshots)
     else
@@ -112,7 +100,8 @@ object BiopetPlugin extends AutoPlugin {
       pushChanges
     )
   }
-  private def biopetCleanSiteFilter: FileFilter = {
+  private def biopetCleanSiteFilter: Def.Initialize[FileFilter] =
+  Def.setting {
     new FileFilter {
       def accept(f: File) = {
         if (isSnapshot.value) {
@@ -124,7 +113,8 @@ object BiopetPlugin extends AutoPlugin {
       }
     }
   }
-  private def biopetGenerateDocsFunction(): Unit = {
+  private def biopetGenerateDocsFunction(): Def.Initialize[Task[Unit]] =
+    Def.task[Unit] {
     if (biopetIsTool.value) {
       import Attributed.data
       val r = (runner in Runtime).value
@@ -143,7 +133,8 @@ object BiopetPlugin extends AutoPlugin {
         .foreach(sys.error)
     }
   }
-  private def biopetGenerateReadmeFunction(): Unit = {
+  private def biopetGenerateReadmeFunction(): Def.Initialize[Task[Unit]] =
+    Def.task[Unit] {
     if (biopetIsTool.value) {
       import sbt.Attributed.data
       val r: ScalaRun = (runner in Runtime).value
