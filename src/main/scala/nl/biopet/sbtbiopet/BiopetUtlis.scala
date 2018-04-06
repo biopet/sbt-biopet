49774d1f1eb75d1aa745da2aa39c458d4758a898
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2018-04-06 15:15:53 +0200

    create crude markdown chapter extractor

--- src/main/scala/nl/biopet/sbtbiopet/BiopetUtlis.scala
+++ src/main/scala/nl/biopet/sbtbiopet/BiopetUtlis.scala
@@ -7,9 +7,32 @@ import scala.io.Source
 object BiopetUtlis {
   def markdownExtractChapter(markdownFile: File, chapter: String): String = {
     val text = Source.fromFile(markdownFile).getLines().toList
-    val chapters = splitStringList(text,line => line.startsWith("#"))
-
-    ""
+    val chapterRegex = "(^#+)([\\t ]*)(.*)$".r("hashtags","whitespace","heading")
+    val allChapters = chapterRegex.findAllIn(text.mkString("\n")).matchData.toList
+    val chapterLine = allChapters.find(r => r.group("heading")== chapter)
+    val headingDepth = chapterLine match {
+      case Some(r) => r.group("hashtags").length
+      case _ => throw new Exception("Chapter not found")
+    }
+    val regex = s"(^#{1,$headingDepth})([\\t ]*)(.*)$$".r
+    def matcher(string: String): Boolean = {
+      string match {
+        case regex => true
+        case _ => false
+      }
+    }
+    val chapters = splitStringList(text, matcher)
+    val correctChapter = chapters.find(lines => lines.headOption match {
+      case Some(line) => chapterRegex.findFirstMatchIn(line) match {
+        case Some(matcherObject) => matcherObject.group("heading") == chapter
+        case _ => false
+      }
+      case _ => false
+    })
+    correctChapter match {
+      case Some(x) => x.mkString("\n")
+      case _ => throw new Exception()
+    }
   }
 
   def splitStringList(stringList:List[String], splitter: String => Boolean): List[List[String]] = {
